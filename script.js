deac592d339bd21b57ea04d9a438decec25b17ed
isabebelly <isabelly.hypolito.santos@escola.pr.gov.br>
2026-08-04 08:42:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome;
+let idade;
+let mes;
+
+nome = prompt("Qual o seu nome?");
+idade = prompt ("Qual sua idade?");
+mes = prompt ("qual mẽs você nasceu?")
+
+alert("Hello, " + nome + " Você tem " + idade + " anos " + " e você nasceu em " + mes );
