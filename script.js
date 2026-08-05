eaf35f5c118427387ebda4cb0d0090a593f49b5d
isabebelly <isabelly.hypolito.santos@escola.pr.gov.br>
2026-08-05 10:21:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let nome;
 let idade;
 let mes;
+let numero1;
+let numero2;
+let resultado;
+
+
 
 nome = prompt("Qual o seu nome?");
 idade = prompt ("Qual sua idade?");
-mes = prompt ("qual mẽs você nasceu?")
+mes = prompt ("qual mês você nasceu?");
+
 
 alert("Hello, " + nome + " Você tem " + idade + " anos " + " e você nasceu em " + mes );
+ numero1 = Number (prompt("digite o primeiro numero"));
+ numero2 = Number (prompt ("digite o segundo numero"));
+ resultado = numero1 + numero2;
+ alert ("o resultado da SOMA é: " + resultado )
+
